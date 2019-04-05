f3a55ec74980c2f221412a7fc7c1469a0111be7b
Cristian Arrieta <crisap94@users.noreply.github.com>
2019-04-05 11:29:00 -0500

    Update MHZ19_getppm.ino

--- examples/MHZ19_getppm/MHZ19_getppm.ino
+++ examples/MHZ19_getppm/MHZ19_getppm.ino
@@ -2,36 +2,50 @@
     MH-Z19 CO2 sensor  SAMPLE
   ----------------------------------------------------------*/
 
-#include <MHZ19_uart.h>
+#include "MHZ19.h"
 
-const int rx_pin = 4;	//Serial rx pin no
-const int tx_pin = 5;	//Serial tx pin no
+const int rx_pin = 13; //Serial rx pin no
+const int tx_pin = 15; //Serial tx pin no
 
-MHZ19_uart mhz19;
+const int pwmpin = 14;
+
+MHZ19 *mhz19_uart = new MHZ19(rx_pin,tx_pin);
+MHZ19 *mhz19_pwm = new MHZ19(pwmpin);
 
 /*----------------------------------------------------------
     MH-Z19 CO2 sensor  setup
   ----------------------------------------------------------*/
-void setup() {
-  Serial.begin(9600);
-  mhz19.begin(rx_pin, tx_pin);
-  mhz19.setAutoCalibration(false);
-  while( mhz19.isWarming() ) {
-    Serial.print("MH-Z19 now warming up...  status:");Serial.println(mhz19.getStatus());
-    delay(1000);
-  }
+void setup()
+{
+    Serial.begin(115200);
+    mhz19_uart->begin(rx_pin, tx_pin);
+    mhz19_uart->setAutoCalibration(false);
+    while (mhz19_uart->isWarming())
+    {
+        Serial.print("MH-Z19 now warming up...  status:");
+        Serial.println(mhz19_uart->getStatus());
+        delay(1000);
+    }
 }
 
 /*----------------------------------------------------------
     MH-Z19 CO2 sensor  loop
   ----------------------------------------------------------*/
-void loop() {
-  int co2ppm = mhz19.getPPM();
-  int temp = mhz19.getTemperature();
+void loop()
+{
+    int co2ppm = mhz19_uart->getPPM(MHZ19_POTOCOL::UART);
+    int temp = mhz19_uart->getTemperature();
 
-  Serial.print("co2: "); Serial.println(co2ppm);
-  Serial.print("temp: "); Serial.println(temp);
-  
-  delay(5000);
+    Serial.print("co2: ");
+    Serial.println(co2ppm);
+    Serial.print("temp: ");
+    Serial.println(temp);
+
+    co2ppm = mhz19_pwm->getPPM(MHZ19_POTOCOL::PWM);
+    Serial.print("co2: ");
+    Serial.println(co2ppm);
+    
+
+    delay(5000);
 }
 
