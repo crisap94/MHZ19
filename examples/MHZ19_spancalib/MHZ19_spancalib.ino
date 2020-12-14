8925cfc6ebca4900d47ab6fa0f8988b7cf22b8d6
Cristian Arrieta <crisap94@users.noreply.github.com>
2020-12-13 19:15:13 -0500

    Update MHZ19_spancalib.ino

--- examples/MHZ19_spancalib/MHZ19_spancalib.ino
+++ examples/MHZ19_spancalib/MHZ19_spancalib.ino
@@ -19,10 +19,7 @@ void setup() {
   mhz19.begin(rx_pin, tx_pin);
 
   mhz19.setAutoCalibration(false);
-  while ( mhz19.isWarming() ) {
-    Serial.print("MH-Z19 now warming up...  status:"); Serial.println(mhz19.getStatus());
-    delay(1000);
-  }
+  delay(3000);
   Serial.println();
 }
 
